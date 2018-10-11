113540c2a7ce51144f67be1311e55166914469b3
Frederick-S <mao_xiaodan@hotmail.com>
2018-10-11 08:29:04 +0800

    Init

---
+++ src/main/kotlin/nhk/controller/IndexController.kt
@@ -0,0 +1,10 @@
+package nhk.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController : BaseController() {
+    @GetMapping("/")
+    fun index() = "index"
+}
