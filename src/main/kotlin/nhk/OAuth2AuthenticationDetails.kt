138df5b0391d92e9ca94e419ee80535404746bf6
Frederick-S <mao_xiaodan@hotmail.com>
2018-10-15 08:26:27 +0800

    Add OAuth2AuthenticationDetails

---
+++ src/main/kotlin/nhk/OAuth2AuthenticationDetails.kt
@@ -0,0 +1,13 @@
+package nhk
+
+class OAuth2AuthenticationDetails {
+    var accessToken = ""
+
+    var tokenType = ""
+
+    var refreshToken = ""
+
+    var expiresIn = 0
+
+    var scope = ""
+}
