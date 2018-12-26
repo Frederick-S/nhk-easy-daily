5f6097e7b56f9f88b844100257e924a70067a62f
Frederick-S <mao_xiaodan@hotmail.com>
2018-12-26 23:13:48 +0800

    Add NHKTopNews

---
+++ src/main/kotlin/nhk/NHKNewsEasy.kt
@@ -0,0 +1,9 @@
+package nhk
+
+import nhk.domain.NHKTopNews
+
+object NHKNewsEasy {
+    public fun getTopNewsForToday(): List<NHKTopNews> {
+        return emptyList()
+    }
+}
---
+++ src/main/kotlin/nhk/domain/NHKTopNews.kt
@@ -0,0 +1,43 @@
+package nhk.domain
+
+import java.util.*
+
+class NHKTopNews {
+    val topPriorityNumber = 0
+
+    val topDisplayFlag = false
+
+    val newsId = ""
+
+    val newsPrearrangedTime = Date()
+
+    val title = ""
+
+    val titleWithRuby = ""
+
+    val outlineWithRuby = ""
+
+    val newsFileVer = false
+
+    val newsPublicationStatus = false
+
+    val hasNewsWebImage = false
+
+    val hasNewsWebMovie = false
+
+    val hasNewsEasyImage = false
+
+    val hasNewsEasyMovie = false
+
+    val hasNewsEasyVoice = false
+
+    val newsWebImageUri = ""
+
+    val newsWebMovieUri = ""
+
+    val newsEasyImageUri = ""
+
+    val newsEasyMovieUri = ""
+
+    val newsEasyVoiceUri = ""
+}
