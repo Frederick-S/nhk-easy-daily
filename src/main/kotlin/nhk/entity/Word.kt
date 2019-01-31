a7c981b4c2b3ff2bd37a4ae99f7ca164c9084e2d
Frederick-S <mao_xiaodan@hotmail.com>
2019-01-31 22:45:02 +0800

    Add unique to column

--- src/main/kotlin/nhk/entity/Word.kt
+++ src/main/kotlin/nhk/entity/Word.kt
@@ -1,9 +1,11 @@
 package nhk.entity
 
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
 class Word : BaseEntity() {
+    @Column(unique = true)
     var name = ""
 
     var definition = ""
