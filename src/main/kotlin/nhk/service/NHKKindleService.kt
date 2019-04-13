1af36a260a7a8cb3725364139dcbb731b5c45a87
Frederick-S <mao_xiaodan@hotmail.com>
2019-04-13 13:51:01 +0800

    Extract method

--- src/main/kotlin/nhk/service/NHKKindleService.kt
+++ src/main/kotlin/nhk/service/NHKKindleService.kt
@@ -80,7 +80,13 @@ class NHKKindleService {
         }
 
         val news = paragraphs.joinToString(separator = "")
-        val words = nhkNews.words
+        val words = getWordsHtml(nhkNews)
+
+        return getHtml(nhkNews.title, news, words)
+    }
+
+    private fun getWordsHtml(nhkNews: NHKNews): String {
+        return nhkNews.words
                 .joinToString(separator = "") {
                     val definitions = it.definitions
                             .joinToString("") { definition ->
@@ -99,8 +105,6 @@ class NHKKindleService {
                         </ol>
                     """.trimIndent()
                 }
-
-        return getHtml(nhkNews.title, news, words)
     }
 
     private fun getHtml(title: String, news: String, words: String): String {
