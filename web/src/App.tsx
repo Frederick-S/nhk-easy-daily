e571fb7f57d4d1cfe17529f35baa0a758f5298a7
Frederick-S <mao_xiaodan@hotmail.com>
2019-04-14 15:38:46 +0800

    Lint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
 import Button from '@material-ui/core/Button'
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <Button variant="contained" color="primary">Hello World</Button>
-    );
+    )
   }
 }
 
-export default App;
+export default App
