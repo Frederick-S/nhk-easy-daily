9c2b80272835860485532085850ea6bfe71949d8
Frederick-S <mao_xiaodan@hotmail.com>
2019-04-18 13:18:40 +0800

    Add findByPublishedAtUtcBetween

--- src/main/kotlin/nhk/repository/NHKNewsRepository.kt
+++ src/main/kotlin/nhk/repository/NHKNewsRepository.kt
@@ -2,5 +2,8 @@ package nhk.repository
 
 import nhk.entity.NHKNews
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.Instant
 
-interface NHKNewsRepository : JpaRepository<NHKNews, Int>
+interface NHKNewsRepository : JpaRepository<NHKNews, Int> {
+    fun findByPublishedAtUtcBetween(start: Instant, end: Instant)
+}
--- src/main/kotlin/nhk/service/NHKSenderService.kt
+++ src/main/kotlin/nhk/service/NHKSenderService.kt
@@ -1,10 +1,22 @@
 package nhk.service
 
+import nhk.repository.NHKNewsRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.time.ZonedDateTime
 
 @Service
 class NHKSenderService {
     private val logger: Logger = LoggerFactory.getLogger(NHKKindleService::class.java)
+
+    @Autowired
+    lateinit var nhkNewsRepository: NHKNewsRepository
+
+    @Autowired
+    lateinit var nhkKindleService: NHKKindleService
+
+    fun sendToSubscribers(utcDate: ZonedDateTime) {
+    }
 }
